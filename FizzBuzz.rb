d0d35d2c4a258c57ebcd579bf247c7687c5cfcbc
Takhir78 <t.salimov@gmail.com>
2017-02-24 00:43:23 -0500

    FizzBuzz minitest

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,6 +1,24 @@
 def fizzbuzz(number)
-	1 ..
+	result = []
+
+	 (1..n).each do |i|
+		if i % 15 == 0
+			result.push "FizzBuzz"
+		elsif i % 3 == 0
+			result.push "Fizz"
+		elsif i % 5 == 0
+			result.push "Buzz"
+		else
+			result.push i
+		end
+	end
+
+
+	result
+
 
 end
 
-end
+
+
+
--- test_FizzBuzz.rb
+++ test_FizzBuzz.rb
@@ -3,13 +3,35 @@ require_relative "FizzBuzz.rb"
 
 class Test < Minitest::Test
 
-	def test_pass_1_function_returns_1
-		assert_equal(1, fizzbuzz(1))
+	def test_fizzbuzz_simple
+		assert_equal [1], fizzbuzz(1), "fizzbuzz(1) should return [1]"
 
 	end
 
-	def test_pass_2_function_returns_2
-		assert_equal(2, fizzbuzz(2))
+	def test_fizzbuzz
+		result = fizzbuzz(15)
+
+
+		assert result.kind_of?(Array), "Function should return an array"
+
+		expected = [
+			1, 2, "Fizz", 4, "Buzz",
+			"Fizz", 7, 8, "Fizz", "Buzz",
+			11, "Fizz", 13, 14, "FizzBuzz"
+		]
+
+
+		assert_equal expected.length,
+					result.length,
+					"Function shoul return #{expected.length} objects"
+
+		result.each_with_index do |obj, idx|
+			assert_equal expected[idx],
+						 obj,
+						 "Function should map #{idx + 1} to #{expected[idx]} instead of #{obj}"
+
+		end
+
 	end
 
 end
